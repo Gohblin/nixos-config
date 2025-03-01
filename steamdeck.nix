e13b085b76f6909595ccb589cd4d07ec88dbd17b
Gohblin <literategoblin@gmail.com>
2025-03-01 04:38:01 -0500

    test

--- steamdeck.nix
+++ steamdeck.nix
@@ -4,177 +4,125 @@ let cfg = config.my.steamdeck;
 in {
   options.my.steamdeck = {
     enable = mkEnableOption "Steamdeck-specific setup";
-
-    gamescope = {
-      enable = mkEnableOption "Jovian Steam gamescope session";
-      user = mkOption {
-        type = types.str;
-        default = "deck";
-      };
-      bootSession = mkOption {
-        type = types.str;
-        default = "gamescope-wayland"; # Changed to match Jovian
-      };
-      desktopSession = mkOption {
-        type = types.str;
-        default = "plasma"; # Default desktop session
-      };
-    };
-
-    opensd = {
-      enable = mkEnableOption "Userspace driver for Valve's Steam Deck";
-      user = mkOption { type = types.str; };
+    user = mkOption {
+      type = types.str;
+      default = "deck";
+      description = "User for Steam Deck and gaming mode";
     };
   };
 
-  config = mkMerge [
-    (mkIf cfg.enable {
-      jovian = {
-        hardware.has.amd.gpu = true;
-        devices.steamdeck = {
-          enable = true;
-          enableXorgRotation = false;
-          enableDefaultCmdlineConfig = true;
-          autoUpdate = false;
-        };
-        steamos = {
-          useSteamOSConfig = false;
-          enableDefaultCmdlineConfig = true;
-          enableMesaPatches = false;
-          enableVendorRadv = false;
-        };
-      };
-
-      services.libinput = {
-        enable = true;
-      };
-
-      services.xserver.videoDrivers = [ "amdgpu" ];
-
-      boot.kernelParams = [
-        "tsc=directsync"
-        "module_blacklist=tpm"
-        "spi_amd.speed_dev=1"
-      ];
-
-      hardware.cpu.amd.updateMicrocode = true;
-      hardware.opengl.enable = true;
-
-      services.udev.extraRules = ''
-        KERNEL=="event[0-9]*", ATTRS{phys}=="usb-0000:04:00.4-3/input0", TAG+="kwin-ignore-tablet-mode"
-        KERNEL=="event[0-9]*", ATTRS{name}=="extest fake device", TAG+="kwin-ignore-tablet-mode"
-      '';
-
-      hardware.pulseaudio.enable = mkIf
-        (config.jovian.devices.steamdeck.enableSoundSupport
-          && config.services.pipewire.enable)
-        (mkForce false);
-
-      services.fwupd.enable = true;
-      environment.systemPackages = with pkgs; [
-        steamdeck-firmware
-        jupiter-dock-updater-bin
-        plasmadeck-vapor-theme
-      ];
-    })
-    
-    (mkIf (cfg.enable && cfg.opensd.enable) {
-      users.groups.opensd = { };
-      users.users."${cfg.opensd.user}".extraGroups = [ "opensd" ];
-      services.udev.packages = [ pkgs.opensd ];
-
-      home-manager.users."${cfg.opensd.user}".systemd.user.services.opensd = {
-        Install = { WantedBy = [ "default.target" ]; };
-        Service = { ExecStart = "${getExe pkgs.opensd} -l info"; };
-      };
-    })
-    
-    (mkIf (cfg.enable && cfg.gamescope.enable) {
-      # Set the default session for login
-      services.displayManager.defaultSession = cfg.gamescope.bootSession;
+  config = mkIf cfg.enable {
+    # Basic Steam Deck hardware setup
+    jovian = {
+      # Hardware configuration
+      hardware.has.amd.gpu = true;
       
-      # Add a script to manually start gaming mode
-      environment.systemPackages = with pkgs; [
-        (writeShellScriptBin "start-steam-gamemode" ''
-          #!/bin/sh
-          gamescope -e -- steam -gamepadui -fulldesktopres
-        '')
-      ] ++ [
-        (pkgs.makeAutostartItem rec {
-          name = "steam";
-          package = pkgs.makeDesktopItem {
-            inherit name;
-            desktopName = "Steam";
-            exec = "steam -silent %U";
-            icon = "steam";
-            extraConfig = {
-              OnlyShowIn = "KDE";
-            };
-          };
-        })
-      ];
-
-      jovian = {
-        steam = {
-          enable = true;
-          user = cfg.gamescope.user;
-          autoStart = true;
-          defaultSession = "gamescope-wayland"; # Explicitly set the gaming mode session
-          desktopSession = cfg.gamescope.desktopSession;
-          gameScope = {
-            enable = true;
-            args = ["-e"];  # Ensure gamescope is configured correctly
-            steamArgs = ["-gamepadui"]; # Launch directly into gamepad UI
-          };
-        };
-        decky-loader = {
-          enable = true;
-          user = "root";
-          extraPackages = with pkgs; [
-            curl
-            unzip
-            util-linux
-            gnugrep
-            readline.out
-            procps
-            pciutils
-            libpulseaudio
-          ];
-        };
-      };
-
-      programs.steam = {
+      # Steam Deck specific setup
+      devices.steamdeck = {
         enable = true;
-        extest.enable = true;
-        remotePlay.openFirewall = true;
-        gamescopeSession = {
-          enable = true;
-          args = ["-f"];  # Make sure gamescope session args are set
-        };
+        enableXorgRotation = false; # Unfix X11 rotation (drivers fix it)
+        enableDefaultCmdlineConfig = true;
+        autoUpdate = false;
       };
-
-      # Create a systemd service to auto-start gaming mode for the deck user
-      systemd.user.services.steam-gamemode = {
-        description = "Steam Gaming Mode";
-        wantedBy = [ "graphical-session.target" ];
-        serviceConfig = {
-          Type = "simple";
-          ExecStart = "${pkgs.steam}/bin/steam -gamepadui -fulldesktopres";
-          Restart = "on-failure";
-          RestartSec = "5";
-        };
-      };
-
-      home-manager.users."${cfg.gamescope.user}".home.packages = with pkgs; [
-        unstable.steamtinkerlaunch
-        unstable.protonup-qt
-        unstable.mangohud
-      ];
       
-      # Make sure gamescope package is installed
-      environment.systemPackages = with pkgs; [
-        gamescope
-      ];
-    })
-  ];
+      # SteamOS configuration
+      steamos = {
+        useSteamOSConfig = false;
+        enableDefaultCmdlineConfig = true;
+        enableMesaPatches = false;
+        enableVendorRadv = false;
+      };
+      
+      # Steam and Gamescope configuration (key part from documentation)
+      steam = {
+        enable = true;
+        user = cfg.user;
+        autoStart = true; # Enable auto-start at boot (preferred way per docs)
+        desktopSession = "plasma"; # Switch to Desktop goes to Plasma
+      };
+      
+      # Enable Decky Loader
+      decky-loader = {
+        enable = true;
+        user = "root"; # Recommended by Jovian-NixOS
+        extraPackages = with pkgs; [
+          curl
+          unzip
+          util-linux
+          gnugrep
+          readline.out
+          procps
+          pciutils
+          libpulseaudio
+        ];
+      };
+    };
+
+    # Enable Plasma desktop environment
+    services.xserver.desktopManager.plasma5.enable = true;
+    
+    # Enable regular Steam for desktop mode
+    programs.steam = {
+      enable = true;
+      extest.enable = true; # X11->Wayland SteamInput mapping
+      remotePlay.openFirewall = true;
+      gamescopeSession.enable = true;
+    };
+
+    # GPU drivers and hardware support
+    services.xserver.videoDrivers = [ "amdgpu" ];
+    hardware.opengl.enable = true;
+    hardware.cpu.amd.updateMicrocode = true;
+    
+    # Steam Deck specific kernel parameters
+    boot.kernelParams = [
+      "tsc=directsync"
+      "module_blacklist=tpm"
+      "spi_amd.speed_dev=1"
+    ];
+
+    # Input support
+    services.libinput.enable = true;
+    
+    # Ignore built-in trackpad as a desktop input
+    services.udev.extraRules = ''
+      KERNEL=="event[0-9]*", ATTRS{phys}=="usb-0000:04:00.4-3/input0", TAG+="kwin-ignore-tablet-mode"
+      KERNEL=="event[0-9]*", ATTRS{name}=="extest fake device", TAG+="kwin-ignore-tablet-mode"
+    '';
+
+    # Sound support
+    hardware.pulseaudio.enable = mkIf
+      (config.jovian.devices.steamdeck.enableSoundSupport
+        && config.services.pipewire.enable)
+      (mkForce false);
+    
+    # Firmware and utilities
+    services.fwupd.enable = true;
+    environment.systemPackages = with pkgs; [
+      steamdeck-firmware
+      jupiter-dock-updater-bin
+      plasmadeck-vapor-theme
+      gamescope # Ensure gamescope is installed
+      
+      # Add a helper script for manual gaming mode launch
+      (writeShellScriptBin "start-gaming-mode" ''
+        #!/bin/sh
+        exec start-gamescope-session
+      '')
+    ];
+    
+    # Create a user if it doesn't exist
+    users.users.${cfg.user} = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "video" "input" "audio" "dialout" ];
+      # You may want to set a password or SSH keys here
+    };
+    
+    # Add Steam-related packages to the user
+    home-manager.users.${cfg.user}.home.packages = with pkgs; [
+      mangohud
+      protonup-qt
+      gamemode
+    ];
+  };
 }
