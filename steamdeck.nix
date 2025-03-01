372ce11a32a499c3edd2fdc1cefca1af464c2945
Gohblin <literategoblin@gmail.com>
2025-03-01 04:15:37 -0500

    test

--- steamdeck.nix
+++ steamdeck.nix
@@ -13,11 +13,11 @@ in {
       };
       bootSession = mkOption {
         type = types.str;
-        default = "steam-wayland";
+        default = "gamescope-wayland"; # Changed to match Jovian
       };
       desktopSession = mkOption {
         type = types.str;
-        default = "steam-wayland"; # Placeholder, remember to override
+        default = "plasma"; # Default desktop session
       };
     };
 
@@ -33,14 +33,12 @@ in {
         hardware.has.amd.gpu = true;
         devices.steamdeck = {
           enable = true;
-          enableXorgRotation = false; # Unfix X11 rotation (drivers fix it)
+          enableXorgRotation = false;
           enableDefaultCmdlineConfig = true;
           autoUpdate = false;
         };
         steamos = {
           useSteamOSConfig = false;
-
-          # Cherry-pick changes
           enableDefaultCmdlineConfig = true;
           enableMesaPatches = false;
           enableVendorRadv = false;
@@ -51,37 +49,27 @@ in {
         enable = true;
       };
 
-      # GPU setup
       services.xserver.videoDrivers = [ "amdgpu" ];
 
-      # Missing SteamOS kernelParams
       boot.kernelParams = [
-        "tsc=directsync" # Probably https://bugzilla.kernel.org/show_bug.cgi?id=202525 ?
-        "module_blacklist=tpm" # Second layer of "fuck TPM" :3
+        "tsc=directsync"
+        "module_blacklist=tpm"
         "spi_amd.speed_dev=1"
       ];
 
-      # Fix random hangs possibly?
       hardware.cpu.amd.updateMicrocode = true;
+      hardware.opengl.enable = true;
 
-      # Enable OpenGL
-      hardware.opengl = {
-        enable = true;
-      };
-
-      # Ignore built-in trackpad as a desktop input
       services.udev.extraRules = ''
         KERNEL=="event[0-9]*", ATTRS{phys}=="usb-0000:04:00.4-3/input0", TAG+="kwin-ignore-tablet-mode"
         KERNEL=="event[0-9]*", ATTRS{name}=="extest fake device", TAG+="kwin-ignore-tablet-mode"
       '';
 
-      # Sounds are set up by Jovian NixOS, so we don't want base pulseaudio
       hardware.pulseaudio.enable = mkIf
         (config.jovian.devices.steamdeck.enableSoundSupport
           && config.services.pipewire.enable)
         (mkForce false);
 
-      # Firmware updaters
       services.fwupd.enable = true;
       environment.systemPackages = with pkgs; [
         steamdeck-firmware
@@ -89,56 +77,29 @@ in {
         plasmadeck-vapor-theme
       ];
     })
+    
     (mkIf (cfg.enable && cfg.opensd.enable) {
       users.groups.opensd = { };
-
       users.users."${cfg.opensd.user}".extraGroups = [ "opensd" ];
-
       services.udev.packages = [ pkgs.opensd ];
 
-      # Enable OpenSD service
       home-manager.users."${cfg.opensd.user}".systemd.user.services.opensd = {
         Install = { WantedBy = [ "default.target" ]; };
-
         Service = { ExecStart = "${getExe pkgs.opensd} -l info"; };
       };
     })
+    
     (mkIf (cfg.enable && cfg.gamescope.enable) {
-      services.displayManager.defaultSession = cfg.gamescope.bootSession; # Still in effect with Jovian's dm
-
-      jovian = {
-        steam = {
-          enable = true;
-          user = cfg.gamescope.user;
-          # Session management
-          autoStart = true;
-          inherit (cfg.gamescope) desktopSession;
-        };
-        decky-loader = {
-          enable = true;
-          user = "root"; # https://github.com/Jovian-Experiments/Jovian-NixOS/blob/1171169117f63f1de9ef2ea36efd8dcf377c6d5a/modules/decky-loader.nix#L80-L84
-          extraPackages = with pkgs; [
-            curl
-            unzip
-            util-linux
-            gnugrep
-
-            readline.out
-            procps
-            pciutils
-            libpulseaudio
-          ];
-        };
-      };
-
-      programs.steam = {
-        enable = true;
-        extest.enable = true; # X11->Wayland SteamInput mapping
-        remotePlay.openFirewall = true;
-      };
-
-      # Since extest fixes the keyboard on Wayland, we probably want autostart for Steam
-      environment.systemPackages = [
+      # Set the default session for login
+      services.displayManager.defaultSession = cfg.gamescope.bootSession;
+      
+      # Add a script to manually start gaming mode
+      environment.systemPackages = with pkgs; [
+        (writeShellScriptBin "start-steam-gamemode" ''
+          #!/bin/sh
+          gamescope -e -- steam -gamepadui -fulldesktopres
+        '')
+      ] ++ [
         (pkgs.makeAutostartItem rec {
           name = "steam";
           package = pkgs.makeDesktopItem {
@@ -153,11 +114,66 @@ in {
         })
       ];
 
+      jovian = {
+        steam = {
+          enable = true;
+          user = cfg.gamescope.user;
+          autoStart = true;
+          defaultSession = "gamescope-wayland"; # Explicitly set the gaming mode session
+          desktopSession = cfg.gamescope.desktopSession;
+          gameScope = {
+            enable = true;
+            args = ["-e"];  # Ensure gamescope is configured correctly
+            steamArgs = ["-gamepadui"]; # Launch directly into gamepad UI
+          };
+        };
+        decky-loader = {
+          enable = true;
+          user = "root";
+          extraPackages = with pkgs; [
+            curl
+            unzip
+            util-linux
+            gnugrep
+            readline.out
+            procps
+            pciutils
+            libpulseaudio
+          ];
+        };
+      };
+
+      programs.steam = {
+        enable = true;
+        extest.enable = true;
+        remotePlay.openFirewall = true;
+        gamescopeSession = {
+          enable = true;
+          args = ["-f"];  # Make sure gamescope session args are set
+        };
+      };
+
+      # Create a systemd service to auto-start gaming mode for the deck user
+      systemd.user.services.steam-gamemode = {
+        description = "Steam Gaming Mode";
+        wantedBy = [ "graphical-session.target" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.steam}/bin/steam -gamepadui -fulldesktopres";
+          Restart = "on-failure";
+          RestartSec = "5";
+        };
+      };
+
       home-manager.users."${cfg.gamescope.user}".home.packages = with pkgs; [
         unstable.steamtinkerlaunch
         unstable.protonup-qt
-
         unstable.mangohud
+      ];
+      
+      # Make sure gamescope package is installed
+      environment.systemPackages = with pkgs; [
+        gamescope
       ];
     })
   ];
